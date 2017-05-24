bc7675b8cc0c21329fc0f1fe558bc485123f1858
Wey Gu <littlewey@gmail.com>
2017-05-24 12:37:26 +0800

    stateChecker.py finished first release]

--- lib/stateChecker.py
+++ lib/stateChecker.py
@@ -20,7 +20,7 @@ state = "NA"
 pausedActivityPath = r'C:\uiPath\var\pausedActivity.list'
 inProgressActivityPath = r'C:\uiPath\var\inProgressActivity.var'
 ignoredActivityIdPath = r'C:\uiPath\var\ignoredActivityId.list'
-
+dispatchedActivityPath = r'C:\uiPath\var\dispatchedActivity.list'
 
 with open(pausedActivityPath) as pausedActivityFile:
     pausedActivity = pausedActivityFile.read()
@@ -34,11 +34,16 @@ with open(ignoredActivityIdPath) as ignoredActivityIdFile:
 if ignoredActivityId != "NA":
     ignoredActivityIdList = ignoredActivityId.split()
 
+with open(dispatchedActivityPath) as dispatchedActivityFile:
+    dispatchedActivity = dispatchedActivityFile.read()
+
 
 if inProgressActivity != "NA":
     state = "withInprogressed"
+# Inprogressed are NA from here
 elif pausedActivity != "NA":
     state = "noInprogressed_WithPaused"
+    # considering all paused activities in ignore list case
     if ignoredActivityId != "NA":
         pausedActivitiesList = pausedActivity.split()
         numberOfPausedActivity = len(pausedActivitiesList)
@@ -46,9 +51,14 @@ elif pausedActivity != "NA":
             if activity in ignoredActivityIdList:
                 numberOfPausedActivity = numberOfPausedActivity - 1
         if numberOfPausedActivity <= 0:
-            if 
-            state = "noInprogressedNoPaused_WithDispatched"
-elif 
+            if dispatchedActivity != "NA":
+                state = "noInprogressedNoPaused_WithDispatched"
+# Paused are NA from here
+    # dispatched:Yes
+elif dispatchedActivity != "NA":
+    state = "noInprogressedNoPaused_WithDispatched"
+else:
+    state = "NA"
 
 
 with open(stateCodePath,'w') as stateFile:

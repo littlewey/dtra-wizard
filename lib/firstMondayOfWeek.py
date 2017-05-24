b4caa84e5ab0bf8fd53828c122f85d8ed65dac6a
Wey Gu <littlewey@gmail.com>
2017-05-24 13:43:46 +0800

    hide other windows when not locked windows, without errors when locked windows

--- lib/firstMondayOfWeek.py
+++ lib/firstMondayOfWeek.py
@@ -5,6 +5,6 @@ from datetime import datetime
 import string
 
 firstMondayOfWeekPath = r'C:\uiPath\var\startDate.var'
-firstMondayOfWeek = str(datetime.today().month)+"/"+str(datetime.today().day-datetime.today().weekday())+"/"+str(datetime.today().year)
+firstMondayOfWeek = str(datetime.today().month)+"/"+str(datetime.today().day-datetime.today().weekday())+"/"+str(datetime.today().year) + " "
 with open(firstMondayOfWeekPath,'w') as outputFile:
     outputFile.write(firstMondayOfWeek)

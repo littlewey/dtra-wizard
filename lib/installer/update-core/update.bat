d0ba7fdc8124053d643d416272c3ba8293a551da
Wey Gu <littlewey@gmail.com>
2017-06-06 14:12:44 +0800

    fixing change invoked git path

--- lib/installer/update-core/update.bat
+++ lib/installer/update-core/update.bat
@@ -2,6 +2,6 @@ cd /d C:\
 rmdir upgradeBuffer /S /Q
 mkdir uiPath-updater
 xcopy uiPath\lib\installer\git-bash uiPath-updater /Y /e
-start /wait uiPath-updater\git-cmd.exe git clone http://gitlab.swdp.me/Wey/dtra-wizard upgradeBuffer
+uiPath-updater\cmd\git clone http://gitlab.swdp.me/Wey/dtra-wizard upgradeBuffer
 xcopy upgradeBuffer\* uiPath /Y
 rmdir upgradeBuffer /S /Q

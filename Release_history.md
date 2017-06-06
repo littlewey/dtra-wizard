ddbbc249ea2eb89686f2d2b3f1206ed1dbf8e099
Wey Gu <littlewey@gmail.com>
2017-06-06 14:33:58 +0800

    add release_history.md & build date,time

---
+++ Release_history.md
@@ -0,0 +1,13 @@
+2017/06/06
+- add build release for no-dependency package
+- modify update source from github to gitlab to enhance speed
+- DTRA-update: fixing xcopy switch by adding /e
+- introduce DTRA-update to auto update dtra core via git clone
+2017/06/05
+- Ignore list empty exceptions
+- multi {W6.Web.UI.Controls.W6DataGrid+W6DataGridItemData\n} existed in orginal scarped data
+- missing check existence of activityProgressedDuration.record
+- completeScheduleRate one digit wrong caculation
+- state:1 close window action missing
+- make time window between two activities as 70 seconds
+- add window size adjustment workflow
--- lib/installer/build.cmd
+++ lib/installer/build.cmd
@@ -1,4 +1,7 @@
 
+date /t > c:\uiPath\lib\installer\release\build"
+time /t >> c:\uiPath\lib\installer\release\build"
+
 "C:\Program Files (x86)\WinRAR\winrar" a -r -ep1 -inul -ibck -y -sfx -z"c:\uiPath\lib\installer\config.txt" -x@"c:\uiPath\lib\installer\rar-ignore-list.txt" -iiconc:\uiPath\lib\guiApp\ico\robot\robot.ico c:\uiPath\lib\installer\release\DTRA-Wizard-Setup.exe c:\uiPath 
 
 "C:\Program Files (x86)\WinRAR\winrar" a -r -ep1 -inul -ibck -y -sfx -z"c:\uiPath\lib\installer\config.txt" -x@"c:\uiPath\lib\installer\rar-ignore-list-no-dependency.txt" -iiconc:\uiPath\lib\guiApp\ico\robot\robot.ico c:\uiPath\lib\installer\release\DTRA-Wizard-Setup-no-dependency.exe c:\uiPath 

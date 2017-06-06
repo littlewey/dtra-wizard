eced81189cc4f708c89a1b457020d50c7a18b35f
Wey Gu <littlewey@gmail.com>
2017-06-06 13:45:36 +0800

    modify update source from github to gitlab to enhance speed

--- lib/installer/update-core/update.bat
+++ lib/installer/update-core/update.bat
@@ -2,6 +2,6 @@ cd /d C:\
 rmdir upgradeBuffer /S /Q
 mkdir uiPath-updater
 xcopy uiPath\lib\installer\git-bash uiPath-updater /Y /e
-uiPath-updater\git-cmd.exe git clone https://github.com/littlewey/dtra-wizard upgradeBuffer
+uiPath-updater\git-cmd.exe git clone http://gitlab.swdp.me/Wey/dtra-wizard upgradeBuffer
 xcopy upgradeBuffer\* uiPath /Y
 rmdir upgradeBuffer /S /Q

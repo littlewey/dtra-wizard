6e92a57e018449933af7e8a112da2fbbc9ac36d5
Wey Gu <littlewey@gmail.com>
2017-06-08 11:03:25 +0800

    update updater

--- lib/installer/update-core/update.bat
+++ lib/installer/update-core/update.bat
@@ -3,4 +3,4 @@ rmdir upgradeBuffer /S /Q
 mkdir uiPath-updater
 xcopy uiPath\lib\installer\git-bash uiPath-updater /Y /e /C
 uiPath-updater\cmd\git clone http://gitlab.swdp.me/Wey/dtra-wizard upgradeBuffer
-xcopy upgradeBuffer\* uiPath /Y /e /C /EXCLUDE:Preference_Configuration.csv
+xcopy upgradeBuffer\* uiPath /Y /e /C /EXCLUDE:c:\uiPath\lib\installer\update-core\excludelist
